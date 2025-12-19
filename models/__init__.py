aebe106c2226d1b4b0dc85b527aab66b53011bcc
fione77 <fionne107@gmail.com>
2025-12-19 13:35:28 +0800

    jsdhaf

---
+++ models/__init__.py
@@ -0,0 +1,1 @@
+from .socratic import Socratic
---
+++ models/socratic.py
@@ -0,0 +1,21 @@
+from bigcode_eval.models.base import BaseModel
+from socratic_core import SocraticCodeGenerator
+
+class Socratic(BaseModel):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.generator = SocraticCodeGenerator()
+
+    def generate(self, prompts, max_length, temperature, **kwargs):
+        outputs = []
+
+        for prompt in prompts:
+            problem = {
+                "id": "humaneval",
+                "title": "",
+                "description": prompt
+            }
+            results, _ = self.generator.generate_for_problem(problem)
+            outputs.append(results["code"])
+
+        return outputs
